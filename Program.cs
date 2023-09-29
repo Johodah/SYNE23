3ed9baf438d629aab7192167fa00c34805f18dd1
Johodah <122483649+Johodah@users.noreply.github.com>
2023-09-29 14:51:08 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,72 +1,203 @@
-﻿class Program
+<<<<<<< HEAD
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
+bool isRunning = true;
+int runnerEnergy = 50;
+int coveredDistance = 10;
+bool playAgain = true;
+
+while (isRunning)
 {
+    Console.WriteLine("Välkommen till Running Simulator!");
+    Console.WriteLine("=====================================");
 
-    enum veckodagar
+    Console.WriteLine("Du startar med 50 energipoäng.");
+    Console.WriteLine("Du måste slutföra en löpbana som är 10 km lång.");
+    Console.WriteLine("Du anger din hastighet för varje 1 km i enheterna 1.snabbast (2km), 2. snabb, 3. måttlig, 4. långsam, 5. promenad eller 6. för att avsluta.");
+
+    while (playAgain)
     {
-        Måndag,
-        Tisdag,
-        Onsdag,
-        Torsdag,
-        Fredag,
-        Lördag,
-        Söndag
-    }
-
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Ange ett tal");
-        string number = Console.ReadLine();
-        Console.WriteLine("du skrev: " + number);
-        int y = int.Parse(number);
-        Console.WriteLine("Kvadraten av " + y + " blir ");
-        Console.WriteLine(y * y);
-        //Uppgift 1
-
-        Console.WriteLine("Ange ett tal med decimaler");
-        float x = float.Parse(Console.ReadLine());
-        Console.WriteLine("du skrev in " + x);
-        Console.WriteLine("Kvadratroten av " + x + " är " + Math.Sqrt(x));
-
-        //Uppgift 2
-
-        Console.WriteLine("Hej! Vad heter du?");
-        string name = Console.ReadLine();
-        Console.WriteLine("Hallå " + name + " och välkommen till SYS");
-
-        //Uppgift 3
-
-        bool success = true;
-        Console.WriteLine("Write True or False");
-        success = bool.Parse(Console.ReadLine());
-        Console.WriteLine("You wrote " + success);
-
-        //Uppgift 4
-
-        Console.WriteLine("Skriv en bokstav eller ett tecken");
-        char tecken = Convert.ToChar(Console.ReadLine());
-        Console.WriteLine(tecken);
-        byte ascii = Convert.ToByte(tecken);
-        Console.WriteLine(ascii);
-        //Uppgift 5
-
-        string dateString = "";
-        Console.Write("Skriv in ett datum i ordningen DD/MM/YYYY: ");
-        dateString = Console.ReadLine();
-        DateTime date = new DateTime();
-        date = DateTime.Parse(dateString);
-        Console.WriteLine("Du uppgav " + date.ToString(" dd/MM/YYYY"));
-        //Uppgift 6, årtalet funkar inte? TINTIN: När du kallar på ToString för ett DateTime objekt så är det viktigt med stora bokstäver, så du ska ist för "dd/MM/YYYY" ha "dd/MM/yyyy" :-)
-
-        Console.WriteLine("Säg en veckodag");
-        string UserInput = Console.ReadLine();
-
-        veckodagar veckodagar;
-        if (Enum.TryParse(UserInput, true, out veckodagar));
+        string menu = Console.ReadLine();
+        switch (menu)
         {
-            int numericvalue = (int)veckodagar;
-            Console.WriteLine($"{UserInput} corresponds to the numeric value: {numericvalue}");
-        
-        }
+            case "1":
+                runnerEnergy = runnerEnergy - 15;
+                coveredDistance = coveredDistance - 2;
+                if (runnerEnergy <= 0 || coveredDistance < 0)
+                {
+                    runnerEnergy = 0;
+                    Console.WriteLine("Du misslyckades, fett sämst.");
+                    playAgain = false;
 
+                }
+
+                else if (coveredDistance == 0)
+                {
+                    Console.WriteLine("Du vann! TJoho!!!!!!");
+                }
+                Console.WriteLine($"Du valde snabbast, då har du {runnerEnergy} kvar och {coveredDistance} km kvar.");
+
+                break;
+            case "2":
+                runnerEnergy = runnerEnergy - 10;
+                coveredDistance = coveredDistance - 1;
+                if (runnerEnergy <= 0 || coveredDistance < 0)
+                {
+                    runnerEnergy = 0;
+                    Console.WriteLine("Du misslyckades, fett sämst.");
+                    playAgain = false;
+
+                }
+
+                else if (coveredDistance == 0)
+                {
+                    Console.WriteLine("Du vann! TJoho!!!!!!");
+                }
+                Console.WriteLine($"Du valde snabb, då har du {runnerEnergy} kvar och {coveredDistance} km kvar.");
+                break;
+            case "3":
+                runnerEnergy = runnerEnergy - 8;
+                coveredDistance = coveredDistance - 1;
+                if (runnerEnergy <= 0 || coveredDistance < 0)
+                {
+                    runnerEnergy = 0;
+                    Console.WriteLine("Du misslyckades, fett sämst.");
+                    playAgain = false;
+
+                }
+
+                else if (coveredDistance == 0)
+                {
+                    Console.WriteLine("Du vann! TJoho!!!!!!");
+                }
+                Console.WriteLine($"Du valde måttlig, då har du {runnerEnergy} kvar och {coveredDistance} km kvar.");
+                break;
+            case "4":
+                runnerEnergy = runnerEnergy - 5;
+                coveredDistance = coveredDistance - 1;
+                if (runnerEnergy <= 0 || coveredDistance < 0)
+                {
+                    runnerEnergy = 0;
+                    Console.WriteLine("Du misslyckades, fett sämst.");
+                    playAgain = false;
+
+                }
+
+                else if (coveredDistance == 0)
+                {
+                    Console.WriteLine("Du vann! TJoho!!!!!!");
+                }
+                Console.WriteLine($"Du valde långsam, då har du {runnerEnergy} kvar och {coveredDistance} km kvar.");
+                break;
+            case "5":
+                runnerEnergy = runnerEnergy - 2;
+                coveredDistance = coveredDistance - 1;
+                if (runnerEnergy <= 0 || coveredDistance < 0)
+                {
+                    runnerEnergy = 0;
+                    Console.WriteLine("Du misslyckades, fett sämst.");
+                    playAgain = false;
+
+                }
+
+                else if (coveredDistance == 0)
+                {
+                    Console.WriteLine("Du vann! TJoho!!!!!!");
+                }
+                Console.WriteLine($"Du valde promenad, då har du {runnerEnergy} kvar och {coveredDistance} km kvar.");
+                break;
+            case "6":
+                Console.WriteLine("Då stänger vi av, tack och hej leverpastej!");
+                System.Environment.Exit(6);
+                isRunning = false;
+                break;
+
+            default:
+                Console.WriteLine("välj ett av valen 1-6, tackar tackar");
+                break;
+
+                playAgain = false;
+                System.Environment.Exit(6);
+
+
+        }
     }
 }
+
+
+// snabbast = 15 energy, 2km
+// snabb = 10 energy, 2km
+// måttlig = 8 energy, 1km
+// långsam = 5 energy, 1km
+// promenad = 2 energy, 1km
+
+=======
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
+bool isRunning = true;
+int runnerEnergy = 50;
+int coveredDistance = 10;
+
+while (isRunning)
+    {
+        Console.WriteLine("Välkommen till Running Simulator!");
+        Console.WriteLine("=====================================");
+
+        Console.WriteLine("Du startar med 50 energipoäng.");
+        Console.WriteLine("Du måste slutföra en löpbana som är 10 km lång.");
+        Console.WriteLine("Du anger din hastighet för varje 1 km i enheterna 1.snabbast (2km), 2. snabb, 3. måttlig, 4. långsam, 5. promenad eller 6. för att avsluta.");
+    string menu = Console.ReadLine();
+    switch (menu)
+    {
+        case "1":
+            runnerEnergy = runnerEnergy - 15;
+            coveredDistance = coveredDistance - 2;
+            Console.WriteLine($"Du valde snabbast, då har du {runnerEnergy} kvar och {coveredDistance} km kvar.");
+            break;
+        case "2":
+            runnerEnergy = runnerEnergy - 10;
+            coveredDistance = coveredDistance - 1;
+            Console.WriteLine($"Du valde snabb, då har du {runnerEnergy} kvar och {coveredDistance} km kvar.");
+            break;
+        case "3":
+            runnerEnergy = runnerEnergy - 8;
+            coveredDistance = coveredDistance - 1;
+            Console.WriteLine($"Du valde måttlig, då har du {runnerEnergy} kvar och {coveredDistance} km kvar.");
+            break;
+        case "4":
+            runnerEnergy = runnerEnergy - 5;
+            coveredDistance = coveredDistance - 1;
+            Console.WriteLine($"Du valde långsam, då har du {runnerEnergy} kvar och {coveredDistance} km kvar.");
+            break;
+        case "5":
+            runnerEnergy = runnerEnergy - 2;
+            coveredDistance = coveredDistance - 1;
+            Console.WriteLine($"Du valde promenad, då har du {runnerEnergy} kvar och {coveredDistance} km kvar.");
+            break;
+        case "6":
+            Console.WriteLine("Då stänger vi av, tack och hej leverpastej!");
+            System.Environment.Exit(6);
+            break;
+
+        default:
+            Console.WriteLine("välj ett av valen 1-6, tackar tackar");
+            break;
+
+            isRunning = false;
+            System.Environment.Exit(6);
+
+
+    }
+    
+    }
+
+
+// snabbast = 15 energy, 2km
+// snabb = 10 energy, 2km
+// måttlig = 8 energy, 1km
+// långsam = 5 energy, 1km
+// promenad = 2 energy, 1km
+
+>>>>>>> 3486cc2839d08ebc7f461ef4ccc319e9bf70073a
