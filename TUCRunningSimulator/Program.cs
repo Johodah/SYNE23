f07d15a03a3ea361bafdffbd8146c082b8c14e96
Johnny Dahle <Johnny.Dahle@hotmail.com>
2023-09-29 11:26:01 +0200

    Add project files.

---
+++ TUCRunningSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Välkommen till Running Simulator!");
+Console.WriteLine("=====================================");
+
+Console.WriteLine("Du startar med 50 energipoäng.");
+Console.WriteLine("Du måste slutföra en löpbana som är 10 km lång.");
+Console.WriteLine("Du anger din hastighet för varje 1 km i enheterna (snabb, måttlig, långsam, promenad): ");
+
+while (true);
+
+   
+
