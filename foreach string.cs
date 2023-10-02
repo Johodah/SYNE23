1f6e285d6b94c8059643d98a26726a7e9eca8250
Johodah <122483649+Johodah@users.noreply.github.com>
2023-10-02 13:14:34 +0200

    Add files via upload

---
+++ foreach string.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string [] techniques = { "C#", "Databaser", "Webbutveckling", "Clean Code" };
+
+        foreach (string l in techniques)
+        {
+            Console.WriteLine(l);
+        }
+    }
+}
