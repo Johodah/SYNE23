a4793d05da6f59abe4d02cab0ae85322e46ec1e2
Johodah <122483649+Johodah@users.noreply.github.com>
2023-10-03 15:21:44 +0200

    Add files via upload

---
+++ Array2.cs
@@ -0,0 +1,13 @@
+﻿internal class Array2
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 12, 50, 72 };
+        int summa = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            summa += numbers[i];
+        }
+        Console.WriteLine("summan av alla talen blir " + summa);
+    }
+}
